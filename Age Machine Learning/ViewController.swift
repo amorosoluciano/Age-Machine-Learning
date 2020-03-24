3ac8bce84853d11383e723604203d971a14007a8
Luciamo <amorosodev@gmail.com>
2020-03-24 14:51:49 +0100

    Initial Commit

---
+++ Age Machine Learning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Age Machine Learning
+//
+//  Created by Luciano Amoroso on 24/03/2020.
+//  Copyright © 2020 Luciano Amoroso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Age Machine LearningTests/Age_Machine_LearningTests.swift
@@ -0,0 +1,34 @@
+//
+//  Age_Machine_LearningTests.swift
+//  Age Machine LearningTests
+//
+//  Created by Luciano Amoroso on 24/03/2020.
+//  Copyright © 2020 Luciano Amoroso. All rights reserved.
+//
+
+import XCTest
+@testable import Age_Machine_Learning
+
+class Age_Machine_LearningTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
